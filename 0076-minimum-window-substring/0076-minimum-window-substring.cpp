50429c2b42d1911d764f722986527904ef71d770
Ansh310 <98263130+Ansh310@users.noreply.github.com>
2025-06-11 00:34:41 +0530

    Time: 14 ms (42.79%), Space: 11.6 MB (44.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    bool check(unordered_map<char, int>& s1, unordered_map<char, int>& s2) {
-        for(auto pair : s1) {
-            if(s1[pair.first] > s2[pair.first]) return false;
-        }
-        return true;
-    }
-
     string minWindow(string s, string t) {
-        int n = s.size(), m = t.size(), st = 0, mini = INT_MAX, ans_st = 0, ans_end = -1;
-        unordered_map<char, int> s1, s2;
-        
-        for(int i = 0; i < m; i++) s1[t[i]]++;
+        int n = s.size(), st = 0, end = 0, counter = t.size(), minSt = 0, minLen = INT_MAX;
+        unordered_map<char, int> m;
 
-        for(int i = 0; i < n; i++) {
-            s2[s[i]]++;
+        for(auto ch : t) {
+            m[ch]++;
+        }
 
-            while(check(s1, s2)) {
-                if(i - st + 1 < mini) {
-                    ans_st = st;
-                    ans_end = i;
-                    mini = i - st + 1;
+        while(end < n) {
+            if(m[s[end]] > 0) counter--;
+
+            m[s[end]]--;
+            end++;
+
+            while(counter == 0) {
+                if(end - st < minLen) {
+                    minSt = st;
+                    minLen = end - st;
                 }
-                s2[s[st]]--;
+                m[s[st]]++;
+
+                if(m[s[st]] > 0) counter++;
+
                 st++;
             }
-       }
+        }
 
-       return s.substr(ans_st, ans_end - ans_st + 1);
+        if(minLen != INT_MAX) return s.substr(minSt, minLen);
+        return "";
+        
     }
 };
