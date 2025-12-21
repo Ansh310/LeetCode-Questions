7680b59704bfc0bcd14e6517e2d513b1193cc1a4
Ansh310 <98263130+Ansh310@users.noreply.github.com>
2025-12-21 21:26:42 +0530

    Time: 0 ms (100%), Space: 12.9 MB (38.2%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    bool check1(vector<string>& strs, int j, int i, unordered_set<int>& indices) {
-        if(i == 0) return true;
-        for(int k = i - 1; k >= 0; k--) {
-            if(indices.find(k) == indices.end() && strs[j - 1][k] < strs[j][k]) {
-                return false;
-            }
-            
-        }
-        if(strs[j - 1][i] > strs[j][i]) return true;
-        return false;
-    }
-    bool check(vector<string>& strs, int i, unordered_set<int>& indices) {
-        for(int j = 1; j < strs.size(); j++) {
-            
-            if(strs[j - 1][i] > strs[j][i]) {
-                if(check1(strs, j, i, indices))
-                return false;
-
-                
-
-            }
-        }
-        return true;
-    }
-
     int minDeletionSize(vector<string>& strs) {
-        int i = 0;
-        unordered_set<int> indices;
+        int count = 0;
+//we can check pairwise also
+        vector<bool> sorted(strs.size() - 1, false);
 
-        while(i < strs[0].size()) {
-            bool cond = check(strs, i, indices);
-            if(!cond) indices.insert(i);
-            i++;
+        for(int i = 0; i < strs[0].size(); i++) {
+            bool check = true;
+
+            for(int j = 0; j < strs.size() - 1; j++) {
+                if(!sorted[j] && strs[j][i] > strs[j + 1][i]) {
+                    check = false;
+                    break;
+                }
+            }
+
+            if(check == false) {
+                count++;
+                continue;
+            }
+
+            for(int j = 0; j < strs.size() - 1; j++) {
+                if(!sorted[j] && strs[j][i] < strs[j + 1][i]) {
+                    sorted[j] = true;
+                }
+            }
+
         }
-        return indices.size();
+        return count;
+
+        
     }
 };
