e83c0d043fd65e235a3a47290e7300aec3b1c511
Ansh310 <98263130+Ansh310@users.noreply.github.com>
2026-07-09 21:46:01 +0530

    Time: 16 ms (61.1%), Space: 224 MB (90.41%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -2,43 +2,22 @@ class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
 
-        int i = 0, j = 0, st = 0;
-        unordered_map<int, int> farthestNode;
+        vector<int> comp(n);
+        comp[0] = 0;
 
-        while(i < n) {
-
-            while(j < n && nums[j] - nums[i] <= maxDiff) j++;
-
-            i++;
-
-            if(i == j) {
-                for(int k = st; k < i; k++) {
-                    farthestNode[k] = i - 1;
-                }
-                st = i;
-            }
-        }
-
-        if(i == j) {
-            for(int k = st; k < i; k++) {
-                farthestNode[k] = i - 1;
-            }
-            st = i;
-        }
-
-        int q = queries.size();
-        vector<bool> ans(q, false);
-
-        for(int i = 0; i < q; i++) {
-            int node1 = queries[i][0];
-            int node2 = queries[i][1];
-
-            if(node1 < node2) {
-                if(farthestNode[node1] >= farthestNode[node2]) ans[i] = true;
+        for(int i = 1; i < n; i++) {
+            if(nums[i] - nums[i - 1] <= maxDiff) {
+                comp[i] = comp[i - 1];
             }
             else {
-                if(farthestNode[node2] >= farthestNode[node1]) ans[i] = true;
+                comp[i] = comp[i - 1] + 1;
             }
+        }
+        int q = queries.size();
+        vector<bool> ans(q);
+
+        for(int i = 0; i < q; i++) {
+            ans[i] = (comp[queries[i][0]] == comp[queries[i][1]]);
         }
         return ans;
     }
