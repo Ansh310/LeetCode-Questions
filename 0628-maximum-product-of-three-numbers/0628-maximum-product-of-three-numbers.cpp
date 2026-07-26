265633f1f6ba1fb15de0da100197c424cd710b0e
Ansh310 <98263130+Ansh310@users.noreply.github.com>
2026-07-27 00:36:09 +0530

    Time: 3 ms (65.12%), Space: 31.5 MB (46.18%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -4,15 +4,14 @@ public:
         int fl = INT_MIN, sl = INT_MIN, tl = INT_MIN, top1 = INT_MAX, top2 = INT_MAX;
 
         for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] < 0) {
-                if(nums[i] < top1) {
+            if(nums[i] < top1) {
                     top2 = top1;
                     top1 = nums[i];
-                }
-                else if(nums[i] < top2) {
-                    top2 = nums[i];
-                }
             }
+            else if(nums[i] < top2) {
+                top2 = nums[i];
+            }
+
             if(nums[i] > fl) {
                 tl = sl;
                 sl = fl;
@@ -24,13 +23,8 @@ public:
             }
             else if(nums[i] > tl) tl = nums[i];
         }
-
-        if(fl < 0) return fl * sl * tl;
-
-        if(top1 < INT_MAX && top2 < INT_MAX) {
-            return max(top1 * top2 * fl, fl * sl * tl);
-        }
-
-        return fl * sl * tl;
+        
+        return max(top1 * top2 * fl, fl * sl * tl);
+        
     }
 };
